7e878e92a68ee36dd6bb9131b226a9600b5c080d
jack <h_7357@qq.com>
2017-12-19 17:55:16 +0800

    add the code

---
+++ code/运算符.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+print('\'2 + 3\': %d' % (2 + 3))
+
+print('\'2 * 3\': %d' % (2 * 3))
+
+print('\'5 / 3\': %f' % (5 / 3))
+
+print('\'5 %% 3\': %d'% (5 % 3))
+
+print('\'3 ** 4\': %d' % (3 ** 4))
+
+print('\'4 // 3\': %d' % (4 // 3))
+
+a = 2
+
+print('\'a = 2\': %d' % a)
+
+a += 2
+
+print('\'a = 2, a += 2\': %d' % a)
+
+a /= 2
+
+print('\'a = 4, a /= 2\': %d' % a)
+
+a = 5
+
+a %= 3
+
+print('\'a = 5, a %%= 3\': %d' % a)
+
+print('\'True and True\': %r' % (True and True))
+
+print('\'True and False\': %r' % (True and False))
+
+print('\'True or False\': %r' % (True or False))
+
+print('\'False or False\': %r' % (False or False))
+
+print('\' 5 >> 2\': %d' % (5 >> 2))
+
+print('\' 5 << 2\': %d' % (5 << 2))
+
+print('\' ~60 \': %d' % (~60))
+
+print('\' 16 | 60 \': %d' % (16 | 60))
+
+print('\' 16 ^ 60 \': %d' % (16 ^ 60))
